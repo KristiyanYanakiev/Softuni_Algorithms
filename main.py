f5828664e5389e610e02a25331ef66c0f4ea1bd1
KristiyanYanakiev <yankris_92@yahoo.com>
2024-11-19 22:00:17 +0200

    Added solution

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+def gen01(idx, vector):
+    if idx >= len(vector):
+        print(vector)
+        return
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    for num in range(2):
+        vector[idx] = num
+        gen01(idx + 1, vector)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+n = 3
+vector = [None] * n
+gen01(0, vector)
